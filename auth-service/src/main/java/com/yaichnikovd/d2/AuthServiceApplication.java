8629c7bb389aab2debb5821e4aad49bcc7cf3b24
Denis Iaichnikov <denis_iaichnikov@epam.com>
2021-03-07 05:33:31 +0300

    Postgres helm chart implementation

---
+++ auth-service/src/main/java/com/yaichnikovd/d2/AuthServiceApplication.java
@@ -0,0 +1,11 @@
+package com.yaichnikovd.d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
