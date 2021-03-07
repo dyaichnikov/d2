9e0ebdabeb7dc38307fba4e90185ace84a83cc7a
Гуров Георгий <magnezia98@mail.ru>
2021-03-07 22:12:36 +0300

    d2 responsive ui

---
+++ d2-ui/source/js/blocks/site-blocks/footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="container footer">
+      <p className="footer__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure obcaecati quas quae ut perspiciatis. Suscipit amet non nulla, porro sit corporis ad ab enim ipsum dolores minus pariatur atque eos?</p>
+    </footer>
+  );
+}
---
+++ d2-ui/source/js/blocks/universal/blocks/logo/logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../../../../../img/logo-placeholder.png';
+
+export default function Logo() {
+  return (
+    <a href=" " className="col-auto logo" title="Logo">
+      <img src="assets/img/logo-placeholder.png" alt="Logo" />
+    </a>
+  );
+}
