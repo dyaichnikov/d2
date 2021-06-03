278307bd788553cec7e3cdcc4adb49b6176029c8
Denis Iaichnikov <denis_iaichnikov@epam.com>
2021-06-04 01:35:04 +0300

    Remove d2-ui module

--- d2-ui/source/js/blocks/site-blocks/main/user/about-user/mock/mock.js
+++
@@ -1,7 +0,0 @@
-const mock = [
-  {
-    accountInformation: [{ publicationsCount: '1 235', subscribersCount: 80000, subscriptions: 65 }],
-  },
-];
-
-export default mock;
--- d2-ui/source/js/blocks/site-blocks/main/user/publication-list/data/data.js
+++
@@ -1,3 +0,0 @@
-const data = ['Publications', 'Marks'];
-
-export default data;
